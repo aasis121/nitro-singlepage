fe9c4149c71b7d5baabdad439f43688a9d947fd7
aasis121 <aashish.rigorousweb@gmail.com>
2020-02-28 11:28:43 +0545

    first commit

---
+++ wp-content/themes/Nitro/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Nitro &mdash; Website Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/aos.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/style.css">
+    <?php wp_head() ?>
+
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+  
+  
+
+
+
+
+
